4c0f1464c7bf4d694b8e79c60a3075de923585bd
unknown <joshuafrellt@gmail.com>
2025-12-17 13:06:54 +0800

    feat: basic grid

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+export default function Square() {
+  return (
+    <>
+      <div>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>1</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>2</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>3</button>
+      </div>
+
+      <div>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>4</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>5</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>6</button>
+      </div>
+
+      <div>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>7</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>8</button>
+        <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>9</button>
+      </div>
+    </>
+  )
+}
